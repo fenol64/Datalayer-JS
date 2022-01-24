a456e7c5e7f5f76e9188bd2a7a92177af088f16d
nox-0202 <fernandinho.nco@gmail.com>
2022-01-24 17:34:50 -0300

    starting build querys

--- src/dataLayer.js
+++ src/dataLayer.js
@@ -1,29 +1,41 @@
 import Connect from "./Connect.js";
 import CrudTrait from "./CrudTrait.js";
 
-export class dataLayer extends CrudTrait {
+export default class DataLayer extends CrudTrait {
   conn = null;
 
-  constructor(entity, required, primary = "id", timestamps = true) {
+  constructor(entity, required = [], primary = "id", timestamps = true) {
+
     this.entity = entity;
     this.required = required;
     this.primary = primary;
     this.timestamps = timestamps;
+
+    this.conn = new Connect("mysql");
+
+    return this;
   }
 
-  save() {
-
+  save(input_insert) {
+    this.sql = `INSERT INTO ${this.entity} (${Object.keys(input_insert).join(', ')}) VALUES (${Object.values(input_insert).join(', ')})`;
+    return this;
   }
 
   find() {
+    this.sql = `SELECT ${this.columns ?? '*'} FROM ${this.entity} ${this.joins} ${this.where}`;
+    return this;
+  }
 
+  findById(id) {
+    this.sql = `SELECT ${this.columns ?? '*'} FROM ${this.entity} ${this.joins} WHERE ${this.primary} = ${id}`;
+    return this;
   }
 
   update() {
 
   } 
 
-  destroy() {
+  destroy(destroy = false) {
 
   }
 
@@ -35,12 +47,3 @@ export class dataLayer extends CrudTrait {
 
 
 
-
-//   Connect() {
-//     this.conn = new Connect("mysql", {
-//       pass: "!sec@1234",
-//       database: "sci",
-//     });
-
-//     return this;
-//   }
